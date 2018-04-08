9b5a769917b82b5b000bad47e9c97e14ef821345
haniwaman <haniwa008@gmail.com>
2018-04-08 23:22:52 +0900

    browser-sync追加

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var cssdeclsort = require('css-declaration-sorter');
 var mqpacker = require('css-mqpacker');
 var plumber = require('gulp-plumber');
 var notify = require('gulp-notify');
+var browserSync = require('browser-sync');
 
 gulp.task('sass', function() {
 	return gulp.src('./sass/**/*.scss')
@@ -19,4 +20,22 @@ gulp.task('sass', function() {
 
 gulp.task('watch', function() {
   gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('browser-sync', function() {
+	browserSync.init({
+			server: {
+					baseDir: "./",
+					index: "index.html"
+			}
+	});
+});
+
+gulp.task('bs-reload', function () {
+	browserSync.reload();
+});
+
+gulp.task('default', ['browser-sync', 'watch'], function () {
+	gulp.watch("./*.html", ['bs-reload']);
+	gulp.watch("./css/*.css", ['bs-reload']);
 });
