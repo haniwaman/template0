e947c0b5b8d7ca4161b7ca3e1b88e9551ad9e317
haniwaman <haniwa008@gmail.com>
2018-04-11 14:43:39 +0900

    gulp-sass-glob追加 めっちゃ楽になった

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,19 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var postcss = require('gulp-postcss');
+var gulp         = require('gulp');
+var sass         = require('gulp-sass');
+var plumber      = require('gulp-plumber');
+var notify       = require('gulp-notify');
+var sassGlob     = require('gulp-sass-glob');
+var browserSync  = require('browser-sync');
+
+var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
-var cssdeclsort = require('css-declaration-sorter');
-var mqpacker = require('css-mqpacker');
-var plumber = require('gulp-plumber');
-var notify = require('gulp-notify');
-var browserSync = require('browser-sync');
+var cssdeclsort  = require('css-declaration-sorter');
+var mqpacker     = require('css-mqpacker');
 
 gulp.task('sass', function() {
 	return gulp.src('./sass/**/*.scss')
 		.pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
+		.pipe(sassGlob())
 		.pipe(sass({outputStyle: 'expanded'}))
 		.pipe(postcss([autoprefixer()]))
 		.pipe(postcss([cssdeclsort({order: 'alphabetically'})]))
@@ -19,7 +22,7 @@ gulp.task('sass', function() {
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+	gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 gulp.task('browser-sync', function() {
