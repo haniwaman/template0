89761ef3a983c001291d3c68ddcb71b6bb70cecb
haniwaman <haniwa008@gmail.com>
2018-08-08 00:25:55 +0900

    gulp 画像圧縮

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,31 @@
-var gulp         = require('gulp');
-var sass         = require('gulp-sass');
-var plumber      = require('gulp-plumber');
-var notify       = require('gulp-notify');
-var sassGlob     = require('gulp-sass-glob');
-var mmq          = require('gulp-merge-media-queries');
-var browserSync  = require('browser-sync');
+var gulp             = require('gulp');
+var sass             = require('gulp-sass');
+var plumber          = require('gulp-plumber');
+var notify           = require('gulp-notify');
+var sassGlob         = require('gulp-sass-glob');
+var mmq              = require('gulp-merge-media-queries');
+var browserSync      = require('browser-sync');
+var imagemin         = require('gulp-imagemin');
+var imageminPngquant = require('imagemin-pngquant');
+var imageminMozjpeg  = require('imagemin-mozjpeg');
 
 var postcss      = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssdeclsort  = require('css-declaration-sorter');
 var mqpacker     = require('css-mqpacker');
+
+var imageminOption = [
+	imageminPngquant({ quality: '65-80' }),
+	imageminMozjpeg({ quality: 80 }),
+	imagemin.gifsicle({
+		interlaced: false,
+		optimizationLevel: 1,
+		colors: 256
+	}),
+	imagemin.jpegtran(),
+	imagemin.optipng(),
+	imagemin.svgo()
+]
 
 gulp.task('sass', function() {
 	return gulp.src('./sass/**/*.scss')
@@ -44,4 +60,10 @@ gulp.task('default', ['browser-sync', 'watch'], function () {
 	gulp.watch("./*.html", ['bs-reload']);
 	gulp.watch("./css/*.css", ['bs-reload']);
 	gulp.watch("./js/*.js", ['bs-reload']);
+});
+
+gulp.task('imagemin', function () {
+	return gulp.src('./img/base/*.{png,jpg,gif,svg}')
+		.pipe(imagemin(imageminOption))
+		.pipe(gulp.dest('./img'))
 });
