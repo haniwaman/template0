1699ece685d62d2d03ff8ed20aa6d5b2d82b39a9
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-04-23 14:08:33 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -63,7 +63,7 @@ gulp.task( 'default', [ 'browser-sync', 'watch' ], function() {
 
 gulp.task( 'imagemin', function() {
 	return gulp
-		.src( './img/base/*.{png,jpg,gif,svg}' )
+		.src( './img/**/*' )
 		.pipe( imagemin( imageminOption ) )
 		.pipe( gulp.dest( './img' ) );
 });
