d11fbab6fad4d1c1caecc8a9dfa2d86cb5b0f664
haniwaman <32994211+haniwaman@users.noreply.github.com>
2019-05-10 18:31:54 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
 jQuery( '.drawer-item a' ).on( 'click', function() {
 	jQuery( '#drawer-check' ).prop( 'checked', false );
 });
+
+jQuery( '.drawer-open, .drawer-close' ).on( 'click', function() {
+	jQuery( '.drawer-open' ).toggleClass( 'm_checked' );
+	jQuery( '.drawer-close' ).toggleClass( 'm_checked' );
+	jQuery( '.drawer-content' ).toggleClass( 'm_checked' );
+});
